c33a0dd2ef6351f679d5e6415fccbc1efe6a12f7
mattfwilson <mattfwilson@gmail.com>
2022-11-07 01:58:49 -0500

    Added addition, equals, and clear buttons.

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -21,9 +21,12 @@ button7 = Button(root, text='7', padx=40, pady=20, command=button_add)
 button8 = Button(root, text='8', padx=40, pady=20, command=button_add)
 button9 = Button(root, text='9', padx=40, pady=20, command=button_add)
 button0 = Button(root, text='0', padx=40, pady=20, command=button_add)
+button_addition = Button(root, text='+', padx=39, pady=20, command=button_add)
+button_equal = Button(root, text='=', padx=91, pady=20, command=button_add)
+button_clear = Button(root, text='Clear', padx=79, pady=20, command=button_add)
+
 
 # put buttons on screen
-
 button1.grid(row=3, column=0)
 button2.grid(row=3, column=1)
 button3.grid(row=3, column=2)
@@ -37,5 +40,8 @@ button8.grid(row=1, column=1)
 button9.grid(row=1, column=2)
 
 button0.grid(row=4, column=0)
+button_addition.grid(row=5, column=0)
+button_equal.grid(row=5, column=1, columnspan=2)
+button_clear.grid(row=4, column=1, columnspan=2)
 
 root.mainloop()
