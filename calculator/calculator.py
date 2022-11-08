adb5adcaee9737f70876cc910f7bdd2898d38bd3
mattfwilson <mattfwilson@gmail.com>
2022-11-08 02:08:41 -0500

    Number input and clearing works.

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -3,28 +3,31 @@ from tkinter import *
 root = Tk()
 root.title('Calculatorer')
 
-e = Entry(root, width=35, borderwidth=5)
+e = Entry(root, width=40, borderwidth=5)
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
-def button_add():
-    return
+def button_click(num):
+    current = e.get()
+    e.delete(0, END)
+    e.insert(0, str(current) + str(num))
+
+def clear():
+    e.delete(0, END)
 
 # define buttons
-
-button1 = Button(root, text='1', padx=40, pady=20, command=button_add)
-button2 = Button(root, text='2', padx=40, pady=20, command=button_add)
-button3 = Button(root, text='3', padx=40, pady=20, command=button_add)
-button4 = Button(root, text='4', padx=40, pady=20, command=button_add)
-button5 = Button(root, text='5', padx=40, pady=20, command=button_add)
-button6 = Button(root, text='6', padx=40, pady=20, command=button_add)
-button7 = Button(root, text='7', padx=40, pady=20, command=button_add)
-button8 = Button(root, text='8', padx=40, pady=20, command=button_add)
-button9 = Button(root, text='9', padx=40, pady=20, command=button_add)
-button0 = Button(root, text='0', padx=40, pady=20, command=button_add)
-button_addition = Button(root, text='+', padx=39, pady=20, command=button_add)
-button_equal = Button(root, text='=', padx=91, pady=20, command=button_add)
-button_clear = Button(root, text='Clear', padx=79, pady=20, command=button_add)
-
+button1 = Button(root, text='1', padx=40, pady=20, command=lambda: button_click(1))
+button2 = Button(root, text='2', padx=40, pady=20, command=lambda: button_click(2))
+button3 = Button(root, text='3', padx=40, pady=20, command=lambda: button_click(3))
+button4 = Button(root, text='4', padx=40, pady=20, command=lambda: button_click(4))
+button5 = Button(root, text='5', padx=40, pady=20, command=lambda: button_click(5))
+button6 = Button(root, text='6', padx=40, pady=20, command=lambda: button_click(6))
+button7 = Button(root, text='7', padx=40, pady=20, command=lambda: button_click(7))
+button8 = Button(root, text='8', padx=40, pady=20, command=lambda: button_click(8))
+button9 = Button(root, text='9', padx=40, pady=20, command=lambda: button_click(9))
+button0 = Button(root, text='0', padx=40, pady=20, command=lambda: button_click(0))
+button_addition = Button(root, text='+', padx=39, pady=20, command=lambda: button_click())
+button_equal = Button(root, text='=', padx=91, pady=20, command=lambda: button_click())
+button_clear = Button(root, text='Clear', padx=79, pady=20, command=clear)
 
 # put buttons on screen
 button1.grid(row=3, column=0)
@@ -41,7 +44,7 @@ button9.grid(row=1, column=2)
 
 button0.grid(row=4, column=0)
 button_addition.grid(row=5, column=0)
-button_equal.grid(row=5, column=1, columnspan=2)
 button_clear.grid(row=4, column=1, columnspan=2)
+button_equal.grid(row=5, column=1, columnspan=2)
 
 root.mainloop()
