07b0699018f6b27f86d675244ae7e61653caf26d
mattfwilson <mattfwilson@gmail.com>
2022-11-11 17:27:13 -0500

    Renamed files and addes in an image and exit button.

---
+++ app/my_app.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from PIL import ImageTk,Image
+
+root = Tk()
+root.title('My First App')
+root.iconbitmap('smile.ico')
+
+my_img = ImageTk.PhotoImage(Image.open('matt.png'))
+my_label = Label(image=my_img)
+my_label.pack()
+
+button_quit = Button(root, text='Exit', command=root.quit)
+button_quit.pack()
+
+root.mainloop()
