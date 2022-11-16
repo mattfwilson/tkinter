b175300d94b4a2ebec82742866aa0f2e5f5fef77
mattfwilson <mattfwilson@gmail.com>
2022-11-16 01:51:20 -0500

    Fixed bug that was not allowing window to render.

--- invest/compound.py
+++ invest/compound.py
@@ -2,13 +2,13 @@ import tkinter as tk
 
 root = tk.Tk()
 
-root.geometry('600x400')
+root.geometry('300x100')
 
-principal = tk.IntVar()
+base_principal = tk.IntVar()
 int_rate = tk.IntVar()
 
 def calc_int():
-    principal = principal.get()
+    principal = base_principal.get()
     interest = int_rate.get()
 
     print(f'Your principal is: {principal}.')
@@ -17,13 +17,13 @@ def calc_int():
     principal.set('')
     int_rate.set('')
 
-principal_label = tk.Label(root, text='Principal:' font=('Times', 10, 'bold'))
-principal_entry = tk.Entry(root, textvariable=principal, font=('Times', 10, 'normal'))
+principal_label = tk.Label(root, text='Principal:')
+principal_entry = tk.Entry(root, textvariable=base_principal)
 
-interest_label = tk.Label(root, text='Interest:', font=('Times', 10, 'bold'))
-interest_entry = tk.Entry(root, textvariable=int_rate, font=('Times', 10, 'normal'))
+interest_label = tk.Label(root, text='Interest:')
+interest_entry = tk.Entry(root, textvariable=int_rate)
 
-submit_button = tk.Button(root, test='Submit', command='submit')
+submit_button = tk.Button(root, text='Submit', command='submit')
 
 principal_label.grid(row=0, column=0)
 principal_entry.grid(row=0, column=1)
