7c8933b38ba0dbedd685dd174b99ca3ead5ebf6f
mattfwilson <mattfwilson@gmail.com>
2022-11-18 00:51:59 -0500

    Added name/main if condition.

--- app/my_app.py
+++ app/my_app.py
@@ -1,15 +1,19 @@
 from tkinter import *
-from PIL import ImageTk,Image
+from PIL import ImageTk, Image
 
-root = Tk()
-root.title('The App of Matt')
-root.iconbitmap('smile.ico')
+def main():
+    root = Tk()
+    root.title('The App of Matt')
+    root.iconbitmap('smile.ico')
 
-my_img = ImageTk.PhotoImage(Image.open('images/matt.png'))
-my_label = Label(image=my_img)
-my_label.pack()
+    my_img = ImageTk.PhotoImage(Image.open('images/matt.png'))
+    my_label = Label(image=my_img)
+    my_label.pack()
 
-button_quit = Button(root, text='Exit', command=root.quit)
-button_quit.pack()
+    button_quit = Button(root, text='Exit', command=root.quit)
+    button_quit.pack()
 
-root.mainloop()
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
