3ef56a8c8c1426d3cd17cae6fdf6775707805063
mattfwilson <mattfwilson@gmail.com>
2022-11-22 00:16:23 -0500

    Put app into if name/main function. Added title to app.

--- invest/compound2.py
+++ invest/compound2.py
@@ -1,32 +1,32 @@
 import tkinter as tk
 
-root = tk.Tk()
-
 principal = 0
 
-def btn_cmd(obj):
+def main():
 
-    #use global variable
+    root = tk.Tk()
+    root.title('Calculatorer')
+
+    txt = tk.Entry(root)
+    txt.pack()
+
+    lblTextVar = tk.StringVar()
+    lbl = tk.Label(root, textvariable=lblTextVar)
+    lbl.pack()
+
+    btn = tk.Button(text="Calculate", command=lambda obj = txt : btn_cmd(obj, lblTextVar))
+    btn.pack()
+
+    root.mainloop()
+
+def btn_cmd(obj, labelText):
+
     global principal
-    #print its unmodified value
+
     print("aVarOutside: " + str(principal))
-    #modify it with what's written in Entry widget
     principal = obj.get()
-
-    #modify lblTextVar, which is essentially modifying Label's text as lblTextVar is its textvariable
-    lblTextVar.set(obj.get())
-
-    #print what's inside Entry
+    labelText.set(obj.get())
     print("Entry: " + obj.get())
 
-txt = tk.Entry(root)
-txt.pack()
-
-lblTextVar = tk.StringVar()
-lbl = tk.Label(root, textvariable=lblTextVar)
-lbl.pack()
-
-btn = tk.Button(text="Calculate", command=lambda obj = txt : btn_cmd(obj))
-btn.pack()
-
-root.mainloop()
+if __name__ == '__main__':
+    main()
