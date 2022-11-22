454577cf8cccb3d175b2198a847eaf3790c0530b
mattfwilson <mattfwilson@gmail.com>
2022-11-22 00:53:36 -0500

    Update some var names to be more descriptive of their use.

--- invest/compound2.py
+++ invest/compound2.py
@@ -2,23 +2,6 @@ import tkinter as tk
 
 principal = 0
 
-def main():
-
-    root = tk.Tk()
-    root.title('Calculatorer')
-
-    txt = tk.Entry(root)
-    txt.pack()
-
-    lblTextVar = tk.StringVar()
-    lbl = tk.Label(root, textvariable=lblTextVar)
-    lbl.pack()
-
-    btn = tk.Button(text="Calculate", command=lambda obj = txt : btn_cmd(obj, lblTextVar))
-    btn.pack()
-
-    root.mainloop()
-
 def btn_cmd(obj, labelText):
 
     global principal
@@ -27,6 +10,23 @@ def btn_cmd(obj, labelText):
     principal = obj.get()
     labelText.set(obj.get())
     print("Entry: " + obj.get())
+
+def main():
+
+    root = tk.Tk()
+    root.title('Compound Calculatorer 1.0')
+
+    input_text = tk.Entry(root)
+    input_text.pack()
+
+    lblTextVar = tk.StringVar()
+    lbl = tk.Label(root, textvariable=lblTextVar)
+    lbl.pack()
+
+    calc_btn = tk.Button(text="Calculate", command=lambda obj = input_text : btn_cmd(obj, lblTextVar))
+    calc_btn.pack()
+
+    root.mainloop()
 
 if __name__ == '__main__':
     main()
