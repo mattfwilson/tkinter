d15920fb74cba2a5ba6c22cf83d9cd674dccd44c
mattfwilson <mattfwilson@gmail.com>
2022-11-23 02:04:35 -0500

    Trying to make a conditional based off of the input value.

--- invest/compound2.py
+++ invest/compound2.py
@@ -1,29 +1,33 @@
 import tkinter as tk
 
 principal = 0
+interest = 0
 
 def btn_cmd(obj, labelText):
 
     global principal
 
-    print("aVarOutside: " + str(principal))
-    principal = obj.get()
-    labelText.set(obj.get())
-    print("Entry: " + obj.get())
+    if obj.get() > 10:
+        print("Principal: " + str(principal))
+        principal = obj.get()
+        labelText.set(obj.get())
+        print("Entry: " + obj.get())
+    else:
+        print('That is a low number...')
 
 def main():
 
     root = tk.Tk()
     root.title('Compound Calculatorer 1.0')
 
-    input_text = tk.Entry(root)
-    input_text.pack()
+    input_principal = tk.Entry(root)
+    input_principal.pack()
 
     lblTextVar = tk.StringVar()
     lbl = tk.Label(root, textvariable=lblTextVar)
     lbl.pack()
 
-    calc_btn = tk.Button(text="Calculate", command=lambda obj = input_text : btn_cmd(obj, lblTextVar))
+    calc_btn = tk.Button(text="Calculate", command=lambda obj = input_principal : btn_cmd(obj, lblTextVar))
     calc_btn.pack()
 
     root.mainloop()
