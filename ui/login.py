c62866b5db2ca37479377ea422b9012f4db1544a
mattfwilson <mattfwilson@gmail.com>
2022-12-09 03:51:35 -0500

    Attempting to get entries to print upon submit.

--- ui/login.py
+++ ui/login.py
@@ -4,19 +4,20 @@ ctk.set_appearance_mode('dark')
 ctk.set_default_color_theme('green')
 
 root = ctk.CTk()
-root.geometry('500x350')
+root.geometry('600x400')
 
 def login():
-    print('Test')
+    print(entry1)
+    print(entry2)
 
 frame = ctk.CTkFrame(master=root)
-frame.pack(pady=20, padx=60, fill='both', expand='True')
+frame.pack(pady=20, padx=100, fill='both', expand='True')
 
 label = ctk.CTkLabel(master=frame, text='Login System')
-label.pack(pady=12, padx=10)
+label.pack(pady=12, padx=100)
 
 entry1 = ctk.CTkEntry(master=frame, placeholder_text='Username')
-entry1.pack(pady=12, padx=10)
+entry1.pack(pady=12, padx=0)
 
 entry2 = ctk.CTkEntry(master=frame, placeholder_text='Password')
 entry2.pack(pady=12, padx=10)
