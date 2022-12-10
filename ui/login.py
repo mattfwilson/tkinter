a4f36a013955e612d62c19b4bfd43f5019261583
mattfwilson <mattfwilson@gmail.com>
2022-12-10 03:46:35 -0500

    Trying to get entries to print on submission.

--- ui/login.py
+++ ui/login.py
@@ -6,9 +6,6 @@ ctk.set_default_color_theme('green')
 root = ctk.CTk()
 root.geometry('600x400')
 
-def login():
-    print(entry1)
-    print(entry27)
 
 frame = ctk.CTkFrame(master=root)
 frame.pack(pady=20,
@@ -25,8 +22,11 @@ entry1.pack(pady=12, padx=0)
 entry2 = ctk.CTkEntry(master=frame, width=200, placeholder_text='Password')
 entry2.pack(pady=12, padx=10)
 
-button = ctk.CTkButton(master=frame, width=100, text='Login', command=login)
-button.pack(pady=12, padx=10)
+def login():
+    print(entry1.widget.get())
+    print(entry2.widget.get())
+    
+button = ctk.CTkButton(master=frame, width=100, text='Show Login', command=login).pack()
 
 checkbox = ctk.CTkCheckBox(master=frame, text='Remember Me?')
 checkbox.pack(pady=12, padx=10)
