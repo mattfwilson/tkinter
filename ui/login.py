82e500f1e2d2584288c29da312728fee6078c904
mattfwilson <mattfwilson@gmail.com>
2022-12-09 19:49:27 -0500

    Resized the entry inputs.

--- ui/login.py
+++ ui/login.py
@@ -8,21 +8,24 @@ root.geometry('600x400')
 
 def login():
     print(entry1)
-    print(entry2)
+    print(entry27)
 
 frame = ctk.CTkFrame(master=root)
-frame.pack(pady=20, padx=100, fill='both', expand='True')
+frame.pack(pady=20,
+    padx=100,
+    fill='both',
+    expand='True',)
 
-label = ctk.CTkLabel(master=frame, text='Login System')
+label = ctk.CTkLabel(master=frame, text='Login System', justify='right')
 label.pack(pady=12, padx=100)
 
-entry1 = ctk.CTkEntry(master=frame, placeholder_text='Username')
+entry1 = ctk.CTkEntry(master=frame, width=200, placeholder_text='Username')
 entry1.pack(pady=12, padx=0)
 
-entry2 = ctk.CTkEntry(master=frame, placeholder_text='Password')
+entry2 = ctk.CTkEntry(master=frame, width=200, placeholder_text='Password')
 entry2.pack(pady=12, padx=10)
 
-button = ctk.CTkButton(master=frame, text='Login', command=login)
+button = ctk.CTkButton(master=frame, width=100, text='Login', command=login)
 button.pack(pady=12, padx=10)
 
 checkbox = ctk.CTkCheckBox(master=frame, text='Remember Me?')
