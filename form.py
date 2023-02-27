a72d4a96d9218bb0f19c10d8c321577cf4343976
mattfwilson <mattfwilson@gmail.com>
2023-02-27 01:25:39 -0500

    Working display of principal value!

--- form.py
+++ form.py
@@ -1,21 +1,32 @@
 import tkinter as tk
- 
-root = tk.Tk()
-root.geometry("300x170")
 
-principal_input = tk.Text(root, height = 1, width = 25)
-l = tk.Label(root, text = "Principal amount:")
-l.config(font =("Courier", 12))
- 
-principal = 0
- 
-b1 = tk.Button(root, text = "Submit",)
-b2 = tk.Button(root, text = "Exit", command = root.destroy)
- 
-l.pack()
-principal_input.pack()
-b1.pack()
-b2.pack()
- 
-principal_input.insert(tk.END, principal)
-tk.mainloop()
+root = tk.Tk()
+root.geometry("250x120")
+root.title('Calculatorer 2.0')
+principal_label = 0
+
+def show_principal(obj):
+    global principal_label
+
+    print("Default: " + str(principal_label))
+    print("Entry: " + obj.get())
+
+    #modify it with what's written in Entry widget
+    principal_label = obj.get()
+
+    #modify principal, which is essentially modifying Label's text as principal is its textvariable
+    principal.set(obj.get())
+
+entry = tk.Entry(root)
+entry.pack()
+entry.place(x=40, y=35)
+
+submit = tk.Button(text="Submit", command=lambda obj=entry : show_principal(obj))
+submit.pack()
+submit.place(x=170, y=32)
+
+principal = tk.StringVar()
+prince_label = tk.Label(root, textvariable=principal)
+prince_label.pack(padx=10, pady=40, side=tk.BOTTOM)
+
+root.mainloop()
